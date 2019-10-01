7699eafec4358efdf2803b46f37df9b1ab549136
RunaroundDK <48739961+RunaroundDK@users.noreply.github.com>
2019-09-30 23:45:40 -0700

    updated program

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 ## Download videos and music from youtube !!
 import argparse
 import subprocess
+
 ## Three diffrent commands
 ## -d = download single youtube video into mp3 format
-## -ply = Download a whole playlist into an mp3 format
 ## -t = download mutiple songs from a txt file
 ##youtube-dl --extract-audio --audio-format mp3 <video URL>
 ##youtube-dl -a video_links.txt
+
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-d", "--download", required=True,
+	parser.add_argument("-d", "--download",
 						help="Download a youtube video into mp3 format.")
-	parser.add_argument("-ply", "--playlist",
-						help="Download a playlist into a mp3 foramt.")
 	parser.add_argument("-t", "--txt",
 						help="Download from a txt file with urls.")
 
@@ -23,8 +22,16 @@ def main():
 		sysoutput = subprocess.call(cmd, shell=True)
 	elif args.txt:
 		print("Downloading from {}".format(args.txt))
-
-
+		cm = "youtube-dl --extract-audio --audio-format mp3 -a " + args.txt
+		sysoutput = subprocess.call(cm, shell=True)
+	else:
+		print ("Something went wrong..")
 
 if __name__== "__main__":
   main()
+
+
+#Links sources
+#https://docs.python.org/3/howto/argparse.html
+#https://www.journaldev.com/16140/python-system-command-os-subprocess-call#python-ossystem-function
+#https://www.slashgeek.net/2016/06/24/5-youtube-dl-tips-might-not-know/
